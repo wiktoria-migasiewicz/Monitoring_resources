1fda1c289632a0d8e093bc6f7e4cd859b74a8b92
Wiktoria Migasiewicz <wiktoria.migasiewicz@gmail.com>
2024-01-03 12:40:12 +0100

    Version 1.0 of GUI

---
+++ safety/collector/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class SystemInformation(models.Model):
+
+    USER = models.CharField(max_length=200)
+    SYSTEM = models.CharField(max_length=200)
+    VERSION = models.CharField(max_length=200)
+    MACHINE = models.CharField(max_length=200)
+    ARCHITECTURE = models.CharField(max_length=200)
+    ENVIRONMENT = models.CharField(max_length=200)
+    HOSTNAME = models.CharField(max_length=200)
+    PROCESSOR = models.CharField(max_length=200)
+
+
+class SoftwareInformation(models.Model):
+    USER = models.CharField(max_length=200)
+    INFO1 = models.CharField(max_length=200)
+    INFO2 = models.CharField(max_length=200)
+    INFO3 = models.CharField(max_length=200)
+
+
+class HardwareInformation(models.Model):
+    USER = models.CharField(max_length=200)
+    CPU_USAGE = models.CharField(max_length=200)
+    CPU_FREQUENCY = models.CharField(max_length=200)
+    RAM = models.CharField(max_length=200)
+    HDD_TOTAL = models.CharField(max_length=200)
